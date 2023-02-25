3b93e737193694cf6ced7939f8554b47494ce1d7
GusFG13 <gustavofg7@hotmail.com>
2023-02-25 17:22:53 -0300

    Implmenting grouping search

--- SalesWebMvc/Models/SalesRecord.cs
+++ SalesWebMvc/Models/SalesRecord.cs
@@ -1,4 +1,5 @@
 ﻿using SalesWebMvc.Models.Enums;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SalesWebMvc.Models
@@ -8,7 +9,9 @@ namespace SalesWebMvc.Models
         //[DatabaseGenerated(DatabaseGeneratedOption.None)] //determina que o BD não gere um id automaticamente
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)] //determina que o BD gere um id automaticamente
         public int Id { get; set; }
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime Date { get; set; }
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double Amount { get; set; }
         public SaleStatus Status { get; set; }
         public Seller Seller { get; set; }
